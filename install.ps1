e601baf1b1cfdd690050ad008c9e11524978406e
Justin Liu <liuj@waikato.ac.nz>
2021-05-25 16:33:01 +1200

    Install docker and VcXsrv on not exist

--- install.ps1
+++ install.ps1
@@ -1,4 +1,10 @@
-winget install 'Docker Desktop'
-winget install VcXsrv
-$DesktopPath = [Environment]::GetFolderPath("Desktop")
-Copy-Item ".\config.xlaunch" -Destination $DesktopPath
+Write-Output "Install VcXsrv and Docker Desktop"
+
+$software = "VcXsrv";
+$installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -eq $software }) -ne $null
+if (-Not $installed) {winget install -h VcXsrv
+    if ($?) {Write-Output "VcXsrv installed successfully"}}
+$software = "Docker Desktop";
+$installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -eq $software }) -ne $null
+if (-Not $installed) {winget install -h 'Docker Desktop'
+    if ($?) {Write-Output "Docker Desktop installed successfully"}}
